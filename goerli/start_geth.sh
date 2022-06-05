0806c2655c52ef4906739707c97eec698453f0b0
Axel <sauerhoefer@shop-werkstatt.com>
2022-06-05 19:44:55 +0200

    disown process from terminal

--- goerli/start_geth.sh
+++ goerli/start_geth.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-exec > >(exec logger -p user.info) 2> >(logger -p user.warn)
+#exec > >(exec logger -p user.info) 2> >(logger -p user.warn)
 
 gth=$(which geth)
-
-echo "geth binary at location: $gth"
 
 data_dir="$(pwd)/data"
 ipc_path="$data_dir/goerli/goerli.ipc"
 logfile="$(pwd)/logs/goerli.log"
 #cfg="$(pwd)/config/goerli.toml"
 
+rm $logfile
+
 #metrics standalone uri http://<metrics.addr>:<metrics.port>:/debug/metrics
 
-geth --goerli --datadir $data_dir --syncmode "snap" --lightkdf --cache 2048  --http --http.addr 0.0.0.0 --ws --pprof  --metrics --metrics.expensive --metrics.addr 0.0.0.0 --metrics.port 6066   --maxpeers 5 --ipcpath $ipc_path & 
+echo $gth
+echo $logfile
+echo $ipc_path
+echo $data_dir
 
-process_pid=$!
-echo "pid: $process_pid, writing logs to $logfile"
-echo $process_pid > geth.pid
+$gth --goerli --datadir $data_dir --syncmode "snap" --lightkdf --cache 2048  --http --http.addr 0.0.0.0 --ws --pprof  --metrics --metrics.expensive --metrics.addr 0.0.0.0 --metrics.port 6066   --maxpeers 5 --ipcpath $ipc_path > $logfile 2>&1  & 
+
+echo $!>geth.pid
+
+gth_pid=$(<geth.pid)
+echo "pid=$gth_pid"
+
+disown -h $gth_pid 
